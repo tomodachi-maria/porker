743238cda9a08ed6351f481ed2fb7a969a89e1af
tomodachi <maria.kawate@gmail.com>
2020-04-17 11:03:54 +0900

    これは動くはず

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+
 Rails.application.routes.draw do
     get "/" => "home#top"
     #homeコントローラのtopアクションを呼び出してください。
     post "/judge" => "home#judge"
-    get "/result" => "home#result"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
