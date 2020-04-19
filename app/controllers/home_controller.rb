739e088e3bba0b49f6e89bc9c098b1a796005bf6
tomodachi <maria.kawate@gmail.com>
2020-04-19 23:54:03 +0900

    コメントいただいた箇所を修正しました。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,12 @@ class HomeController < ApplicationController
   def judge
     @tefuda = HandCheck.new(params[:hand]) #paramsのデータを持ったインスタンスができる。
     @tefuda.errorcheck #@tefudaがerrorcheckメソッドを通る。
-    if @tefuda.error != "any errors"
-      render :error
+    if
+      @tefuda.error != "any errors"
+        render :error
     else
-      @tefuda = HandCheck.new(params[:hand])
       @tefuda.handcheck
-      render :result
+        render :result
     end #ifのend
   end #defのend
 
--- app/services/check.rb
+++ app/services/check.rb
@@ -14,61 +14,33 @@ module Check
     def errorcheck
       hand_ary = @hand.split
       #文字列handを、そのまま配列にする。※ ["S1","S2","S3","S4","S5"]
-      hand_num = @hand.gsub(/S|H|D|C/, "S" => "", "H" => "", "D" => "", "C" => "")
-      #文字列からS/H/D/Cを消して数字だけの文字列に。※ "1 2 3 4 5"
-      hand_empty = hand_num.gsub(/\d/,"")
-      #hand_numからさらに半角数字を消す。※ "    "
+      hand_ary_for_error1 = @hand.split(/ /, -1)
+      #文字列handを配列にするが、第二引数の負数により連続スペースの間も値になる。
 
-      if hand_ary.size != 5 ||
-        @hand.start_with?(" ") ||
-        @hand.end_with?(" ")
-        @error = ERROR1_NOT_ENOUGH
+      if
+        hand_ary.size != 5 ||
+        hand_ary_for_error1.size != 5
+          @error = ERROR1_NOT_ENOUGH
 
-      elsif /\d{3,}/ =~ hand_num ||
-        #3桁以上の数字があったらとりあえずはじく。以下全部１桁または２桁に関しての話。
-        REGEX_0 =~ @hand ||
-        REGEX_123 =~ @hand ||
-        REGEX_456789 =~ @hand ||
-        REGEX_MARK =~ @hand ||
-        hand_empty.match?(/\S/)
-        #hand_emptyに半角スペース以外があることを表す。
-      #--以下、何枚目のカード指定にエラーがあるかを返すためのパート-------------------------------
-        hand_empty_ary = []
-        hand_ary.each do|s|
-          emp = s.gsub(/S|H|D|C|\d/, "S" => "","H" => "","D" => "","C" => "",/\d/ => "")
-          hand_empty_ary.push (emp)
-        end #これでhand_empty_aryは、元の文字列@handから、SHDCと半角数字が消えた配列になる。
+      elsif
+        @hand.match?(REGEX_0||REGEX_123||REGEX_456789||REGEX_MARK||/\d{3,}/||/[^SDHC0-9]/)
+          @error = [ERROR2_UNSUITABLE]
+          hand_ary.each_with_index do |a, idx|
+            b = a.index(REGEX_0)
+            c = a.index(REGEX_123)
+            d = a.index(REGEX_456789)
+            e = a.index(REGEX_MARK)
+            f = a.index(/\d{3,}/)
+            g = a.index(/[^SDHC0-9]/)
+            @error.push("#{idx + 1}#{ERROR2_WHERE_IS_WRONG}(#{hand_ary[idx]})") if b||c||d||e||f||g
+          end
 
-        where_is_unsuitable_ary0 = []
-        hand_ary.each_with_index do |ary, idx|
-          t = ary.index(REGEX_0)
-          u = ary.index(REGEX_123)
-          v = ary.index(REGEX_456789)
-          w = ary.index(REGEX_MARK)
-          where_is_unsuitable_ary0.push(idx + 1) if t || u || v || w
-        end
-        hand_empty_ary.each_with_index do |ary, idx|
-          x = ary.index(/\S/)
-          where_is_unsuitable_ary0.push(idx + 1) if x
-        end #これでwhere_is_unsuitable_ary0では、不適切なカードは何枚目にあるかが順不同の配列になった。
-
-        where_is_unsuitable_ary1 = where_is_unsuitable_ary0.map(&:to_i)
-        where_is_unsuitable_ary = where_is_unsuitable_ary1.sort
-        # where_is_unsuitable_aryでは、何番目のカードが不適切かが「昇順で」書かれていることになる(順番にエラー文を返すため)
-
-        #いよいよ@errorに、エラー文を入れていく。
-        @error = ["半角英字大文字のスート（S,H,D,C）と数字（1〜13）の組み合わせでカードを指定してください。"]
-        where_is_unsuitable_ary.each do |y|
-          "#{y}番目のカード指定文字が不正です。（#{hand_ary[y - 1]}）"
-          @error.push("#{y}番目のカード指定文字が不正です。（#{hand_ary[y - 1]}）")
-        end
-      #-------------------------------------------------------------------------------------
-
-      elsif (hand_ary.count - hand_ary.uniq.count) > 0
-        @error = "カードが重複しています。"
+      elsif
+        (hand_ary.count - hand_ary.uniq.count) > 0
+          @error = ERROR3_SAME_CARDS
 
       else
-        @error = "any errors"
+          @error = "any errors"
       end #ifのend
     end #defのend
 
@@ -76,11 +48,10 @@ module Check
     def handcheck
       #number関連
         hand_num = @hand.gsub(/S|H|D|C/, "S" => "", "H" => "", "D" => "", "C" => "")
-        #文字列からS/H/D/Cを消して数字だけの文字列に。エラー判定にも利用。
-        # "10 1 3 4 1"になる。
+        #文字列からS/H/D/Cを消して数字だけの文字列に。※ "10 1 3 4 1"
         num = hand_num.split.map(&:to_i)
         #num = [10,1,3,4,1] （各要素は数字）みたいになる。ここから配列になっている。
-        num_forstraight= num.sort.reverse
+        num_for_straight= num.sort.reverse
         num_pairs_hash = num.group_by(&:itself).transform_values(&:size)
         #{10=>1, 1=>2, 3=>1, 4=>1}みたいになる。
         nums_pairs_array = num_pairs_hash.map{ |_, value| value }
@@ -95,43 +66,53 @@ module Check
         #{H=>1, D=>2, S=>2}みたいになる。マークごとの枚数を数える。
         marks_pairs = mark_pairs_hash.map{ |_, value| value }
         #[1, 2, 2]みたいになる。同じマークの枚数を配列にする。
-      if num_forstraight[0] == num_forstraight[1] + 1 &&
-        num_forstraight[1] + 1 == num_forstraight[2] + 2 &&
-        num_forstraight[2] + 2 == num_forstraight[3] + 3 &&
-        num_forstraight[3] + 3 == num_forstraight[4] + 4 &&
+      if
+        num_for_straight[0] == num_for_straight[1] + 1 &&
+        num_for_straight[1] + 1 == num_for_straight[2] + 2 &&
+        num_for_straight[2] + 2 == num_for_straight[3] + 3 &&
+        num_for_straight[3] + 3 == num_for_straight[4] + 4 &&
         marks_pairs.max{|a, b| a.to_f <=> b.to_f} == 5
         #最後の条件文は、配列中の最大値が５＝「同じマークが５枚ある」ことを示している。
           @message = RESULT_STRAIGHT_FLASH
 
-      elsif num_forstraight == [13,12,11,10,1] &&
+      elsif
+        num_for_straight == [13,12,11,10,1] &&
         marks_pairs.max{|a, b| a.to_f <=> b.to_f} == 5
           @message = RESULT_STRAIGHT_FLASH
 
-      elsif nums_pairs == [4,1]
+      elsif
+        nums_pairs == [4,1]
           @message = RESULT_FOUR_OF_A_KIND
 
-      elsif nums_pairs == [3,2]
+      elsif
+        nums_pairs == [3,2]
           @message = RESULT_FULLHOUSE
 
-      elsif marks_pairs.max{|a, b| a.to_f <=> b.to_f} == 5
+      elsif
+        marks_pairs.max{|a, b| a.to_f <=> b.to_f} == 5
           @message = RESULT_FLASH
 
-      elsif num_forstraight[0] == num_forstraight[1] + 1 &&
-        num_forstraight[1] + 1 == num_forstraight[2] + 2 &&
-        num_forstraight[2] + 2 == num_forstraight[3] + 3 &&
-        num_forstraight[3] + 3 == num_forstraight[4] + 4
+      elsif
+        num_for_straight[0] == num_for_straight[1] + 1 &&
+        num_for_straight[1] + 1 == num_for_straight[2] + 2 &&
+        num_for_straight[2] + 2 == num_for_straight[3] + 3 &&
+        num_for_straight[3] + 3 == num_for_straight[4] + 4
           @message = RESULT_STRAIGHT
 
-      elsif num_forstraight == [14,5,4,3,2]
+      elsif
+        num_for_straight == [13,12,11,10,1]
           @message = RESULT_STRAIGHT
 
-      elsif nums_pairs == [3,1,1]
+      elsif
+        nums_pairs == [3,1,1]
           @message = RESULT_THREE_OF_A_KIND
 
-      elsif nums_pairs == [2,2,1]
+      elsif
+        nums_pairs == [2,2,1]
           @message = RESULT_TWO_PAIR
 
-      elsif nums_pairs == [2,1,1,1]
+      elsif
+        nums_pairs == [2,1,1,1]
           @message = RESULT_ONE_PAIR
 
       else
